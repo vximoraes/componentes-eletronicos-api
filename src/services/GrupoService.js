45afc602eeef4490b83a104c1f5e6db7f8e83533
Silvio Huan Matheus Grein Cristo Ribeiro <silviohuan@gmail.com>
2025-09-02 22:21:37 -0400

    feat: adicionar contexto de usuário para operações de atualização e exclusão de grupo; atualizar grupos de usuários na semente

--- src/services/GrupoService.js
+++ src/services/GrupoService.js
@@ -25,6 +25,50 @@ class GrupoService {
             })
         }
         const data = this.repository.criar(parsedData)
+        return data
+    }
+
+    async atualizar(parsedData, id, user) {
+        await this.repository.buscarPorId(id)
+        const grupo = await this.repository.buscarPorNome(parsedData.nome, id)
+        await this.verificarGrupo(user, id)
+        if(grupo) {
+            throw new CustomError({
+                statusCode: HttpStatusCodes.CONFLICT.code,
+                errorType: "resourceConflict",
+                field: "Grupos",
+                details: [],
+                customMessage: messages.error.resourceConflict("Grupos", "nome duplicado")
+            })
+        }
+        const usuario = await this.usuarioRepository.buscarPorId(user.id)
+        const grupoUsuario = usuario.toObject()
+        const data = await this.repository.atualizar(id, parsedData)
+        return data
+    }
+    async deletar(id, user){
+
+        await this.repository.buscarPorId(id)
+        await this.verificarGrupo(user, id)
+        const data = this.repository.deletar(id)
+        return data
+    }
+
+    async verificarGrupo(user, id){
+        const usuario = await this.usuarioRepository.buscarPorId(user.id)
+        const grupoUsuario = usuario.toObject()
+        for(const grupo of grupoUsuario.grupos){
+            if(grupo._id.toString() === id){
+                throw new CustomError({
+                statusCode: HttpStatusCodes.FORBIDDEN.code,
+                errorType: "Forbidden",
+                field: "Grupos",
+                details: [],
+                customMessage: "Este grupo não pode ser alterado ou deletado." 
+            })
+
+            }
+        }
     }
 }
 
